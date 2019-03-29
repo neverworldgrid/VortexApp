64253045fdf9c256c076f5c20279d93e7279f82d
Cody Cooper <nolan.perry.sc@gmail.com>
2019-03-29 23:28:46 +0000

    Add First Files

---
+++ Maps/sim.php
@@ -0,0 +1,13 @@
+<?php
+require"../config.php";
+?>
+
+<div class="balloon-content balloon-content-narrow center-align">
+<h3>Nashville</h3>
+Owner: Digital Mayor<br />
+Currency: Gloebit<br />
+Allows HyperGrid Users: Yes<br />
+Grid Location X: 29999 Y: 30000<br>
+<div>HG: login.digiworldz.com:8002:Nashville</div>
+<div>Local: http://slurl.com/secondlife/Nashville</div>
+</div>
---
+++ NeverWorldSearch/databaseinfo.php
@@ -0,0 +1,6 @@
+<?php
+$DB_HOST = "localhost";
+$DB_USER = "ccooper";
+$DB_PASSWORD = "8/22/2003np";
+$DB_NAME = "ossearch";
+?>
